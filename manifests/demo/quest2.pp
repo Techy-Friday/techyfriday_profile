4e5cf309bb9f29537fd04f0814e6935543b10458
Jethro <jethro@itshosted.nl>
2017-10-02 13:01:12 +0200

    updates

---
+++ manifests/demo/quest2.pp
@@ -0,0 +1,71 @@
+#
+class techyfriday_profile::demo::quest2 (
+  String $directory      = 'data',
+  String $file_name      = 'info.txt'
+  String $file_parameter = undef,
+
+){
+
+  case $::facts['os']['family'] {
+    'RedHat': {   $_directory = "/${directory}" }
+    'windows': {  $_directory = "c:/${directory}"}
+    default: {    fail("Sorry ${::facts['os']['family']} is not supported")}
+  }
+
+  if ($directory == 'data') {
+
+    notify { 'Directory data':
+      message => "
+Welcome at quest2!
+
+This application_name is creating a directory '${_directory}' on this server.\n
+Your next task is to change this directory name to 'storage' via a hiera setting and a level that is environment independend.\n
+\n\n
+",
+    }
+  }
+
+  elsif ($directory == 'storage') {
+
+    if ($file_parameter == undef) {
+      notify { 'Directory storage':
+        message => "
+Perfect you changed the directory name to ${_directory} as requested.\n
+\n
+This puppet run now created a file in ${_directory} called ${file_parameter}.\n
+In this file you can find some information but you can also adjust a parameter called 'file_parameter'\n
+Change this setting to any string that you want.\n\n
+",
+      }
+    } else {
+      notify { 'Directory storage with parameter':
+        message => "
+Well done you have changed the file_parameter setting to ${file_parameter}.
+
+Now to see the real power of hiera make a yaml file with different settings for the
+acceptance environment and after that change the customer_environment to acceptance for this server.
+
+",
+      }
+    }
+
+    file { "${_directory}/${file_name}":
+      ensure  => file,
+      content => "
+# This file is manged by Puppet
+
+This file is for customer ${::facts['customer_name']} and build in the environment ${::facts['customer_environment']}\n
+\n
+The server specs are:\n
+Total memory:           ${::facts['memory']['system']['total']}\n
+Number of CPU's:        ${::facts['processors']['physicalcount']}\n
+Number of CPU's cores:  ${::facts['processors']['count']}\n
+CPU type:               ${::facts['processors']['models'][0]}\n
+\n
+\n
+Current value of file_parameter is: ${file_parameter}\n
+\n";
+    }
+
+  }
+}
--- manifests/initial/initial.pp
+++ manifests/initial/initial.pp
@@ -5,8 +5,14 @@ class techyfriday_profile::initial::initial {
   tag 'techyfriday_profile_initial_initial'
 
   notify { 'Initial message':
-    message => "\n\nHi Admin, this server is running at the 'initial' application_name/role.
-Please move this server to\napplication_name: 'demo'\napplication_role: 'quest1'\n\n",
+    message => "
+Hi Admin, this server is running at the 'initial' application_name/role.
+Please move this server to:
+
+application_name: 'demo'
+application_role: 'quest1'
+
+and do some puppet runs (like 2 times) to make sure this server stays at his new application name/role\n\n",
   }
 
 }
